935f261a9448f2035b2f2df63de6618a16399616
bharu610 <bharathir610@gmail.com>
2026-01-08 21:55:26 +0530

    Add files via upload

---
+++ homeassignments/IsPrimeNumber.java
@@ -0,0 +1,25 @@
+package homeassignments;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+		
+		int n = 45;
+		
+	boolean prime = true;
+	
+	for (int i = 2; i < n; i++) {
+		
+		if(n%i==0) {
+			prime = false;
+			break;
+		}
+	}
+	if (prime==true) {
+		System.out.println(n+" is a prime number");
+	}
+	else {
+		System.out.println(n+" is not a prime number");
+	}
+	}
+}
