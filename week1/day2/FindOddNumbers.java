08b33cc545ac82bc5b60ee5384b5c0d1491fd9a7
bharu610 <bharathir610@gmail.com>
2026-01-15 14:27:35 +0530

    Add files via upload

---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=10;
+		
+		for (int i = 1; i <= n; i++) {
+			if(i % 2 != 0)
+			{
+				System.out.println("The odd numbers are "+i);
+			}
+		}
+
+	}
+
+}
